6fd8fd31a23f96b017a0b6a88266b057a1ce20ce
Arianna Masciolini <uzkamascio@gmail.com>
2019-11-27 01:09:49 +0100

    Aggiunti placeholder per 3 di 4 file essenziali del db.

---
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/db/IngredientEntity.kt
@@ -0,0 +1,7 @@
+package com.disorganizzazione.spesapp.db
+
+/* Classe che rappresenta la tabella "INGREDIENTI" (mostrata nell'activity principale).
+*  Class representing the "INGREDIENTS" table (shown in the main activity). */
+
+class IngredientEntity {
+}
---
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/db/SpesAppDB.kt
@@ -0,0 +1,9 @@
+package com.disorganizzazione.spesapp.db
+
+import androidx.room.RoomDatabase
+
+/* Classe base per l'intero database.
+*  Basic class for the entire database. */
+
+abstract class SpesAppDB : RoomDatabase() {
+}
