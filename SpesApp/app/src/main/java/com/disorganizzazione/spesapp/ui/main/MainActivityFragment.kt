e4fffec5a17ab6c980646ff44f2e164481128936
Arianna Masciolini <uzkamascio@gmail.com>
2019-12-18 18:39:51 +0100

    Modificato il fragment principale in modo che usi il layout giusto ecc.

--- SpesApp/app/src/main/java/com/disorganizzazione/spesapp/ui/main/MainActivityFragment.kt
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/ui/main/MainActivityFragment.kt
@@ -8,34 +8,63 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.disorganizzazione.spesapp.IngredientEntity
 import com.disorganizzazione.spesapp.R
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 /**
  * Un fragment usato in entrambe le schede dell'attività principale, contenente una recycler view in cui è mostrata una
  * lista di ingredienti.
  * A fragment used in both tabs of the main activity, containing a recycler view showing a list of ingredients.
  */
+
 class MainActivityFragment : Fragment() {
 
     private lateinit var pageViewModel: PageViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    // l'overriding di onCreate servirà solo se vorremo permettere rotazioni dello schermo
+    // overriding onCreate will only be useful if we want to allow screen rotations
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
             setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
         }
-    }
+    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
+        lateinit var recyclerView: RecyclerView
+        lateinit var viewAdapter: RecyclerView.Adapter<*>
+        lateinit var viewManager: RecyclerView.LayoutManager
+
+        // seleziona il layout manager responsabile di mostrare i singoli elementi
+        // selects the layout manager responsible for showing the individual elements
+        viewManager = LinearLayoutManager(activity)
+
+        // seleziona l'adapter responsabile di creare le view e associarle ai dati del db
+        // selects the adapter responsible for creating the views and binding them to the data in the db
+        val ingredientList = listOf<IngredientEntity>()
+        viewAdapter = IngredientListAdapter(ingredientList)
+
+        // applica il viewManager e l'adapter alla RecyclerView del frammento
+        // applies the viewManager and the adapter to the fragment's RecyclerView
+        recyclerView = ingr_recycler_view.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        // servirà solo come esempio se vorremo permettere rotazioni dello schermo
+        // only useful as an example if we want to allow screen rotations
+        /*val textView: TextView = root.findViewById(R.id.section_label)
         pageViewModel.text.observe(this, Observer<String> {
             textView.text = it
-        })
-        return root
+        })*/
+        return recyclerView
     }
 
     companion object {
