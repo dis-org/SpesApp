6d835cc0a55c225e1661391145e3c6cf5b47d525
Arianna Masciolini <uzkamascio@gmail.com>
2019-12-18 18:40:12 +0100

    Stub per adapter e viewholder lista ingredienti.

---
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/ui/main/IngredientListAdapter.kt
@@ -0,0 +1,20 @@
+package com.disorganizzazione.spesapp.ui.main
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.disorganizzazione.spesapp.IngredientEntity
+
+// class RecyclerViewAdapter(private val bookList: List<Any>): RecyclerView.Adapter<CustomViewHolder>() {
+class IngredientListAdapter(ingredientList: List<IngredientEntity>): RecyclerView.Adapter<IngredientViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/ui/main/IngredientViewHolder.kt
@@ -0,0 +1,7 @@
+package com.disorganizzazione.spesapp.ui.main
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class IngredientViewHolder(val v: View): RecyclerView.ViewHolder(v) {
+}
