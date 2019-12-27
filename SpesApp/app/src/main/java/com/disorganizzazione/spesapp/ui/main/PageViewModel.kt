2d2306b91bce178a0f3b524e078d214e7047d470
Arianna Masciolini <uzkamascio@gmail.com>
2019-12-27 23:30:39 +0100

    Resa utile la classe che fa le magie coi livedata.

--- SpesApp/app/src/main/java/com/disorganizzazione/spesapp/ui/main/PageViewModel.kt
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/ui/main/PageViewModel.kt
@@ -6,11 +6,24 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+/**
+ * Questa classe serivrà soprattutto se un giorno permetteremo agli utenti di ruotare lo schermo,
+ * ma non credo che nessuno ne senta l'esigenza.
+ * This class will be especially useful if, one day, we will allow our users to rotate the screen,
+ * but I doubt anyone needs this feature.
+ */
+
 class PageViewModel : ViewModel() {
 
     private val _index = MutableLiveData<Int>()
     val text: LiveData<String> = Transformations.map(_index) {
         "Hello world from section: $it"
+    }
+
+    // Non so assolutamente quale sia il proposito di questo file ma penso che questa funzione mi dirà in che fragment sto
+    // I literally have no idea what this file is but I think this function will tell me which fragment I'm looking at
+    fun getIndex(): Int? {
+        return _index.value
     }
 
     fun setIndex(index: Int) {
