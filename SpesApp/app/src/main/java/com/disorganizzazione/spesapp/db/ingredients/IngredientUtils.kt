3e761a41cb699d05a74401c6e41aca5bbde93028
Arianna Masciolini <uzkamascio@gmail.com>
2020-05-27 23:24:46 +0200

    Handled exceptions due to duplicates.

--- SpesApp/app/src/main/java/com/disorganizzazione/spesapp/db/ingredients/IngredientUtils.kt
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/db/ingredients/IngredientUtils.kt
@@ -1,12 +1,18 @@
 package com.disorganizzazione.spesapp.db.ingredients
 
+import android.database.sqlite.SQLiteConstraintException
 import com.disorganizzazione.spesapp.db.SpesAppDB
 
 fun moveToStorage(db: SpesAppDB?, ingr: GroceryListEntity) {
     db?.groceryListDAO()?.deleteFromGroceryList(ingr)
     val se = ingr.toStorageEntity()
     se.done = false
+    try {
     db?.storageDAO()?.insertInStorage(se)
+    } catch (e: SQLiteConstraintException) {
+        // I don't think the user needs to know about duplicates
+        println("Skipped ${se.name} because it was in storage already")
+}
 }
 
 fun commitTransactions(db: SpesAppDB?) {
