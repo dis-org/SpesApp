a541bf9b8f37c6f4e4edb13cd754d6d08ad4213e
Arianna Masciolini <uzkamascio@gmail.com>
2020-05-27 23:26:17 +0200

    Improved parts that would have made my FP teacher shiver.

--- SpesApp/app/src/main/java/com/disorganizzazione/spesapp/db/ingredients/IngredientUtils.kt
+++ SpesApp/app/src/main/java/com/disorganizzazione/spesapp/db/ingredients/IngredientUtils.kt
@@ -8,22 +8,16 @@ fun moveToStorage(db: SpesAppDB?, ingr: GroceryListEntity) {
     val se = ingr.toStorageEntity()
     se.done = false
     try {
-    db?.storageDAO()?.insertInStorage(se)
+        db?.storageDAO()?.insertInStorage(se)
     } catch (e: SQLiteConstraintException) {
         // I don't think the user needs to know about duplicates
         println("Skipped ${se.name} because it was in storage already")
-}
+    }
 }
 
 fun commitTransactions(db: SpesAppDB?) {
     val groceryList = db?.groceryListDAO()?.selectAllInGroceryList() ?: emptyList()
-    for (ingr in groceryList) {
-        if (ingr.done)
-            moveToStorage(db,ingr)
-    }
+    groceryList.map { ingr -> if (ingr.done) moveToStorage(db, ingr) }
     val storageList = db?.storageDAO()?.selectAllInStorage() ?: emptyList()
-    for (ingr in storageList) {
-        if (ingr.done)
-            db?.storageDAO()?.deleteFromStorage(ingr)
-    }
+    storageList.map { ingr -> if (ingr.done) db?.storageDAO()?.deleteFromStorage(ingr) }
 }
