9c13843e6ffd44f6f2505c891f00c1c02b6341b8
Arianna Masciolini <uzkamascio@gmail.com>
2020-08-31 20:00:55 +0200

    added db and navbar stuff to main activity

--- SpesApp/app/src/main/java/boh/harisont/spesapp/MainActivity.kt
+++ SpesApp/app/src/main/java/boh/harisont/spesapp/MainActivity.kt
@@ -8,23 +8,56 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import boh.harisont.spesapp.db.SpesAppDB
 import boh.harisont.spesapp.ui.main.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
+
+/**
+ * Main activity,
+ * where the DB is created,
+ * containing the interface for the two tabs containing the lists
+ * and for the bottom navbar
+ */
 
 class MainActivity : AppCompatActivity() {
 
+    var db: SpesAppDB? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // set layout
         setContentView(R.layout.activity_main)
+
+        // setup tabs
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        // get an instance of the DB
+        db = SpesAppDB.getInstance(this)
+
+        // set listeners for navbar
+        commit_btn.setOnClickListener {
+            // TODO: commit db transaction and update (mutable?) list, cf. legacy code
+            Toast.makeText(this, R.string.coming_soon, Toast.LENGTH_LONG).show()
         }
+
+        share_btn.setOnClickListener {
+            Toast.makeText(this, R.string.coming_soon, Toast.LENGTH_LONG).show()
+        }
+
+        query_btn.setOnClickListener {
+            Toast.makeText(this, R.string.coming_soon, Toast.LENGTH_LONG).show()
+        }
+
+        cookbook_btn.setOnClickListener {
+            Toast.makeText(this, R.string.coming_soon, Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
