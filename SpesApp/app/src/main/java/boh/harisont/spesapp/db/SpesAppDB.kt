f168e1256917d8ab2f08db876766528b9992b61e
Arianna Masciolini <uzkamascio@gmail.com>
2020-09-01 00:26:36 +0200

    added db population just to see if I'm breaking anything

--- SpesApp/app/src/main/java/boh/harisont/spesapp/db/SpesAppDB.kt
+++ SpesApp/app/src/main/java/boh/harisont/spesapp/db/SpesAppDB.kt
@@ -5,24 +5,38 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import boh.harisont.spesapp.db.ingredient.IngredientDao
 import boh.harisont.spesapp.db.ingredient.IngredientEntity
 import boh.harisont.spesapp.db.recipe.RecipeDao
 import boh.harisont.spesapp.db.recipe.RecipeEntity
+import kotlin.concurrent.thread
 
 /**
  * The full database
  */
 
-@Database(entities = [IngredientEntity::class, RecipeEntity::class],version = 1)
+// if the DB changes, update version and provide a migration strategy
+@Database(entities = [IngredientEntity::class, RecipeEntity::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class SpesAppDB: RoomDatabase() {
-    abstract fun IngredientDao(): IngredientDao
-    abstract fun RecipeDao(): RecipeDao
+    abstract fun ingredientDao(): IngredientDao
+    abstract fun recipeDao(): RecipeDao
 
     // to make the DB a singleton (I guess, I clearly copy-pasted it)
     companion object {
         private var INSTANCE: SpesAppDB? = null
+
+        // populate the db for testing purposes
+        // TODO: remove if useless
+        private val CALLBACK = object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                thread {
+                    INSTANCE?.ingredientDao()?.insert(IngredientEntity("latte"))
+                }
+            }
+        }
 
         fun getInstance(context: Context): SpesAppDB? {
             if (INSTANCE == null) {
@@ -30,7 +44,10 @@ abstract class SpesAppDB: RoomDatabase() {
                     INSTANCE = Room.databaseBuilder(
                         context.applicationContext,
                         SpesAppDB::class.java,
-                        "SpesAppDB").fallbackToDestructiveMigration().build()
+                        // fallbackToDestructiveMigration destroys the db if you make bad changes
+                        "SpesAppDB").fallbackToDestructiveMigration()
+                        .addCallback(CALLBACK)
+                        .build()
                     println("DATABASE created")
                 }
             }
