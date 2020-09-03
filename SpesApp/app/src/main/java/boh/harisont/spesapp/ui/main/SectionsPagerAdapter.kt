46e6ba2fdf4c4b03087b25789015312dd720783a
Arianna Masciolini <uzkamascio@gmail.com>
2020-09-03 23:41:32 +0200

    removed placeholder fragment

--- SpesApp/app/src/main/java/boh/harisont/spesapp/ui/main/SectionsPagerAdapter.kt
+++ SpesApp/app/src/main/java/boh/harisont/spesapp/ui/main/SectionsPagerAdapter.kt
@@ -6,22 +6,22 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import boh.harisont.spesapp.R
 
+/**
+ * Mostly auto-generated tabs/fragments magic.
+ */
+
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
+    R.string.tab_text_1,
+    R.string.tab_text_2
 )
 
-/**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
- */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
-    : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        // Return a MainActivityFragment (defined as a static inner class below).
+        return MainActivityFragment.newInstance(position + 1)
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
