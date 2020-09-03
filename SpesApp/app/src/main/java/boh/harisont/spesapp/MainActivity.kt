5312d6ffa5fde9a6efae52e708ccd9016ace35cf
Arianna Masciolini <uzkamascio@gmail.com>
2020-09-03 23:40:08 +0200

    removed useless imports from main activity

--- SpesApp/app/src/main/java/boh/harisont/spesapp/MainActivity.kt
+++ SpesApp/app/src/main/java/boh/harisont/spesapp/MainActivity.kt
@@ -1,18 +1,12 @@
 package boh.harisont.spesapp
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
-import boh.harisont.spesapp.db.SpesAppDB
 import boh.harisont.spesapp.ui.main.SectionsPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.concurrent.thread
 
 /**
  * Main activity,
