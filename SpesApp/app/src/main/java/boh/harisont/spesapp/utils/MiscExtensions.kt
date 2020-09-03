3a47102cfaa7ceec1d65e2297dfa43f24bd34608
Arianna Masciolini <uzkamascio@gmail.com>
2020-09-04 00:19:10 +0200

    readded extension functions

---
+++ SpesApp/app/src/main/java/boh/harisont/spesapp/utils/MiscExtensions.kt
@@ -0,0 +1,18 @@
+package boh.harisont.spesapp.utils
+
+import android.widget.EditText
+
+/**
+ * Miscellaneous extension functions used here and there.
+ */
+
+/**
+ * Replaces empty strings with null, leaves nonempty strings unchanged.
+ * Only used in EditText.getValue(), but it was worth it.
+ */
+fun String.nullIfEmpty() = if (this == "") null else this
+
+/**
+ * Gets the String? contained in an EditText.
+ */
+fun EditText.getContent() = this.text.toString().nullIfEmpty()
