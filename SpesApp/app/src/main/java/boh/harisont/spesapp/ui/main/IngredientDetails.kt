b9bf2509cc7b20ea84cef53e1c91856d5667d0b7
Arianna Masciolini <uzkamascio@gmail.com>
2020-10-09 23:13:56 +0200

    created details activity and stuff

--- SpesApp/app/src/main/java/boh/harisont/spesapp/ui/main/IngredientDetails.kt
+++ SpesApp/app/src/main/java/boh/harisont/spesapp/ui/main/IngredientDetails.kt
@@ -2,11 +2,27 @@ package boh.harisont.spesapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import boh.harisont.spesapp.R
 
 class IngredientDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingredient_details)
+        title = resources.getString(R.string.details)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.details_appbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.close_d) {
+            // TODO:
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
