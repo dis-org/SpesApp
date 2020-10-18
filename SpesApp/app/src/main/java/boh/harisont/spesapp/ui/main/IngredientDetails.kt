815ab48b6cd4a2baa6b2b6a72674ee940b1f8b68
Arianna Masciolini <uzkamascio@gmail.com>
2020-10-18 11:06:11 +0200

    #26 implemented delete from appbar in details

--- SpesApp/app/src/main/java/boh/harisont/spesapp/ui/main/IngredientDetails.kt
+++ SpesApp/app/src/main/java/boh/harisont/spesapp/ui/main/IngredientDetails.kt
@@ -6,12 +6,16 @@ import android.text.Editable
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import boh.harisont.spesapp.R
 import boh.harisont.spesapp.db.ingredient.IngredientEntity
 import kotlinx.android.synthetic.main.activity_ingredient_details.*
 
 class IngredientDetails : AppCompatActivity() {
+
+    private lateinit var ingrViewModel: IngredientViewModel
+    private lateinit var ingr: IngredientEntity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +25,13 @@ class IngredientDetails : AppCompatActivity() {
         title = resources.getString(R.string.details)
 
         // get name of ingredient to display, query the db and show it
-        val ingr = intent.getSerializableExtra("INGR") as IngredientEntity
+        ingr = intent.getSerializableExtra("INGR") as IngredientEntity
         ingr_name_d.setText(ingr.name, TextView.BufferType.EDITABLE)
-        use_before_d.setText(ingr.useBefore.toString(), TextView.BufferType.EDITABLE)
+        val useBefore = if (ingr.useBefore != null) ingr.useBefore.toString() else ""
+        use_before_d.setText(useBefore, TextView.BufferType.EDITABLE)
         category_d.setText(ingr.category, TextView.BufferType.EDITABLE)
+
+        ingrViewModel = ViewModelProvider(this).get(IngredientViewModel::class.java)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -34,8 +41,12 @@ class IngredientDetails : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
-        if (id == R.id.close_d) {
-            // TODO: when
+        when (id) {
+            R.id.delete_d -> {
+                ingrViewModel.delete(ingr)
+                Toast.makeText(application, R.string.delete_success,Toast.LENGTH_LONG).show()
+                finish()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
